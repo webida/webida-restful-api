fd35e92548e538f551feefbb448525318f5838c2
김중훈 <jh1977.kim@samsung.com>
2016-07-07 14:34:25 +0900

    integrated builder project
     - now can be built after cloning
     - simplified 'installation' part of README
     - added 'Build for own' part in README

---
+++ builder/clear.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+API_DIR=..
+
+# cleanup output dir before generating files 
+rm -rf $API_DIR/src
+rm -rf $API_DIR/test
+rm -rf $API_DIR/docs 
+
+rm -f $API_DIR/api-spec/swagger.json
+rm -f $API_DIR/api-spec/LICENSE.json
+rm -f $API_DIR/LICENSE
+rm -f $API_DIR/README.md
+rm -f $API_DIR/package.json
+
